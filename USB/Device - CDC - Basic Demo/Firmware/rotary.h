2d909aa702669222f477d58f4b433b1d16f6f996
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-05-15 18:11:26 -0400

    Changed struct added functions

--- USB/Device - CDC - Basic Demo/Firmware/rotary.h
+++ USB/Device - CDC - Basic Demo/Firmware/rotary.h
@@ -12,37 +12,40 @@
 //Internal defines
 
 // Rotary encoder
-#define RE_A RB5
-#define RE_B RB4
+#define RE_A PORTBbits.RB7
+#define RE_B PORTBbits.RB8
+#define NUMBER_OF_TICKS 12 /**<The number of ticks for a complete revolution of the rotary encoder*/
 
 // Switch
-#define SWITCH0	RB3 /**<Define the push button of the rotary encoder*/
+#define SWITCH0	PORTBbits.RB9 /**<Define the push button of the rotary encoder*/
 
 // Overall hardware state
-struct hardwareState {
+struct rotaryHardwareState {
 	
-	// Rotary Encoder position
-	int encoderPosition;
-	unsigned char encoderState;
-	unsigned char encoderPreviousState;
-	int encoderDirection;
-	
-	// Push button
-	unsigned char buttonState;
-	unsigned char buttonDebounce;
+    // Rotary Encoder position
+    int encoderPosition; /**< The postion of the rotary encoder*/
+    unsigned char encoderState;
+    unsigned char encoderPreviousState;
+    int encoderDirection;
 
-}hardware;
+    // Push button
+    unsigned char buttonState;
+    unsigned char buttonDebounce;
 
-//Output defines
-uint8_t TURN_LEFT;
-uint8_t TURN_RIGHT;
-uint8_t PUSH_BUTTON;
-uint8_t NUMBER_OF_TICKS;
+    //State
+    int8_t direction;
+    int8_t position;
 
-void readRotary(void);
+};
+void initRotaryStruct(struct rotaryHardwareState* state);
+/**
+ * @brief A function that reads the current state of the rotary encoder.
+ * @param state The structure containing the current and previous state of the encoder.
+ */
+void readRotary(struct rotaryHardwareState* state);
 
 /*A function that acts on information stored in the hardware struct. Sets a bit high when you turn
 left or right. Also allows for a button push. 1 is considered active.*/
-void rotaryFunction();
+int8_t rotaryFunction();
 
 #endif
