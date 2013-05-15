385c7f60c07386e2b5b06f70d033671f0a49e7e0
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-05-15 10:00:07 -0400

    Initial commit

---
+++ USB/Device - CDC - Basic Demo/Firmware/MPLAB.X/adc.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   adc.h
+ * Author: dduboi4
+ *
+ * Created on May 15, 2013, 9:58 AM
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ADC_H */
+
